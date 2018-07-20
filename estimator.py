bab78fa39bf6ddd7d42cd1fc6ebc6a8f72c70105
Pedro Nuno Ferreira Magalhães <pnfmagalhaes@gmail.com>
2018-07-20 10:20:47 +0100

    Gcode estimator core
    Added the calculation of the amount of filament

--- estimator.py
+++ estimator.py
@@ -1,5 +1,10 @@
 print("###-GCODE-ESTIMATOR-###")
 
+E_last_pos = 0.0
+E_curr_pos = 0.0
+E_extr_wit = 0.0    #Extruder extruded with
+
+
 #open file
 with  open("Cubo_Med_20.gcode","r") as file:
     lines = []                                  #dictionary to save lines
@@ -26,5 +31,26 @@ with  open("Cubo_Med_20.gcode","r") as file:
         for command in ['G','X','Y','Z','E','F']:
             for k in line.keys():
                 if command == k.upper():
-                    print(k + str(line[k]) + ' ',end="")
-        print('')
+                    #print(k + str(line[k]) + ' ',end="")
+                    a=2
+        
+    
+    #calculation of the filament with in mm
+    for line in lines:
+        for k in line.keys():
+            E_curr_pos = line[k]
+            if k.upper()=='E':
+                #print(k + str(line[k]) )
+                if   line[k] > E_last_pos:
+                    #extrusion
+                    E_extr_wit = E_extr_wit + (E_curr_pos - E_last_pos)
+                    E_last_pos = E_curr_pos
+                elif line[k] < E_last_pos:
+                    #retraction
+                    E_extr_wit = E_extr_wit - (E_last_pos - E_curr_pos)
+                    E_last_pos = E_curr_pos
+                    a = 2
+                else:
+                    #stoped
+                    a = 1
+    print("Total amount ou filament in mm:",E_extr_wit)
