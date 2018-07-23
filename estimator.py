474ccbc00b8127b6714474aef3a6a37e5c710c10
Pedro Nuno Ferreira Magalhães <pnfmagalhaes@gmail.com>
2018-07-23 15:58:46 +0100

    estimator core
    now with an aproximation of the impat of the aceleration in small distances

--- estimator.py
+++ estimator.py
@@ -5,21 +5,26 @@ E_last_pos = 0.0
 E_curr_pos = 0.0
 E_extr_wit = 0.0    #Extruder extruded with
 
-F_curr = 0          #current feedrate
-F_next = 0          #next feedrate
+F_curr = 0.01          #current feedrate
+F_next = 100          #next feedrate
 F_update = False
 
+X_last_pos = 0.0
 X_curr_pos = 0.0
 X_next_pos = 0.0
-X_stop = True
+Y_last_pos = 0.0
 Y_curr_pos = 0.0
 Y_next_pos = 0.0
 Z_curr_pos = 0.0
 Z_next_pos = 0.0
+Z_layer_d = 0.0
 XY_dist = 0.0
 
 elapsed_time = 0.0
 elapsed_time_del = 0.0
+deltaX = 0.0
+delta_F = 1.0
+deltaY = 0.0
 
 a = 500             #aceleration = 500mm/s2
 #open file
@@ -75,46 +80,39 @@ with  open("Cubo_Med_20.gcode","r") as file:
 
     #distance calculation in XYZ axis
     for line in lines:
-        if X_stop:
-            # the X axis is stoped and it will start
-            
         for k in line.keys():
             if k.upper() == 'X':
                 #new X next position
-                X_next_pos = line[k]
-                if X_next_pos == X_curr_pos:
-                    #stoped
-                    X_stop = True
-                else:
-                    X_stop = False
+                X_next_pos = line[k]            
                 #print(X_next_pos)
             elif k.upper() == 'Y':
                 #new Y next position
                 Y_next_pos = line[k]
             elif k.upper() == 'Z':
                 #Layer change 
-                Z_next_pos = line[k]
-
+                Z_next_pos = line[k]               
+                Z_layer_d = Z_next_pos-Z_curr_pos
             elif k.upper() == 'F':
                 #new feedrate
                 F_next = line[k]/60
-                F_update = True
+                
+        
         d_XY = math.sqrt( (X_next_pos-X_curr_pos)**2 + (Y_next_pos-Y_curr_pos)**2 )
-
-        if F_curr != 0:
-             
-            if F_update:
-                #movement with diferent speed rate
-                elapsed_time_del = d_XY/F_next
-            else:
-                elapsed_time_del = d_XY/F_curr
-            
-            elapsed_time = elapsed_time + elapsed_time_del
-            XY_dist = XY_dist + d_XY
-
+       
+        if abs(d_XY) < 3:
+            #distancia a percorrer < 2mm
+            vf = math.sqrt(F_curr**2 + a*d_XY)
+            elapsed_time_del = 2*(vf-F_curr)/a
+        else:
+            elapsed_time_del = d_XY/F_next
+        
+        elapsed_time = elapsed_time + elapsed_time_del + Z_layer_d/F_next 
+        XY_dist = XY_dist + d_XY
         #update X and Y current position
+        X_last_pos = X_curr_pos
         X_curr_pos = X_next_pos
         Y_curr_pos = Y_next_pos
+        Z_curr_pos = Z_next_pos
         F_curr     = F_next
         F_update = False
-    print("elapsed time",elapsed_time/60)
+    print("elapsed time in min",elapsed_time/60)
